2e4b1929ee1c54f59b396bacd300080105ee85f5
Oliwier Posiakow <oliwier.posiakow@binarapps.com>
2025-10-04 10:52:24 +0200

    Building app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { assignRoutes } from "@utils/assign-routes";
 
 import express from "express";
 
-import { setupSwagger } from "@config/swagger.ts";
+import { setupSwagger } from "@config/swagger";
 
 const app = express();
 app.use(express.json());
